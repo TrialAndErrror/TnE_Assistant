ab8beec6c0d34da923df84bded3c689b5920fde6
trialanderrror <coding@trialanderrror.com>
2021-01-28 13:04:30 -0500

    Updated default helper.sh file

--- src/bash/helper.sh
+++ src/bash/helper.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# Replace /path/to/TnE_Assistant with the absolute path to the TnE_Assistant folder
+gnome-terminal -e "bash -c \"python3 /path/to/TnE_Assistant/main.py\"" --geometry 73x19+300+200
 
-gnome-terminal -e "bash -c \"python3 /home/wade/PycharmProjects/TnE_Assistant/main.py\"" --geometry 73x19+300+200
+# Running with gnome-terminal to make sure the terminal pops up and prompts the user
