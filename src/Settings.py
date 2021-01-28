2d9c6c8e0cb4d6fb51cccfd3c151ca68cacdf3cb
trialanderrror <coding@trialanderrror.com>
2021-01-28 18:23:30 -0500

    formatting custom intro

--- src/Settings.py
+++ src/Settings.py
@@ -73,12 +73,11 @@ def print_custom_intro():
 
     print('Welcome to Ratty\'s Assistant\n'
           '\n'
-          '\n'
           'You can ask me to play or look up anything!\n'
           '\n'
           'You can Say:\n'
           '\tSearch Maps of Ancient Rome\n'
-          '\tPlay Miley Cyrus - Born in the USA\n'
+          '\tPlay Elton John\n'
           '\tWho is Al Gore?\n'
           '\tFind Car Rentals Near Me\n'
           '\tPlay Chocolate Chip Cookie Recipe No Eggs'
